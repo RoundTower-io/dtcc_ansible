5a24a96a0f3a25da812cbaaa61d64be46d2c2025
gamename <tennis.n.smith@gmail.com>
2019-09-13 07:59:17 -0500

    Document the filter

--- plugins/filter/port_channels.py
+++ plugins/filter/port_channels.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python
+"""
+Given a list of interfaces, return only the port channel interfaces
+"""
+
+
+def port_channels(input_interfaces):
+    """
+    Find (and return) port channels in a collection of interface names
+    :param input_interfaces: A collection of interface names
+    :return: A list of port channel interfaces
+    """
+    ports = []
+    for interface in input_interfaces:
+        interface_lower = str(interface).lower()
+        if interface_lower.startswith('po'):
+            ports.append(interface)
+    return ports
+
 
 class FilterModule(object):
 
     def filters(self):
+        """
+        Associate the filter with a function
+        :return: The filter-to-function association
+        """
         return {
-            'port_channels': self.port_channels
+            'port_channels': port_channels
         }
-
-    def port_channels(self, input_interfaces):
-        ports = []
-        for interface in input_interfaces:
-            if 'port-channel' in str(interface).lower():
-                ports.append(interface)
-        return ports
