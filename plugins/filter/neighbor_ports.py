5c89b0dba915e4a1328e9cb51c3c2fc38f334ce3
gamename <tennis.n.smith@gmail.com>
2019-09-13 08:22:25 -0500

    Document the filter

--- plugins/filter/neighbor_ports.py
+++ plugins/filter/neighbor_ports.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python
-import yaml
+"""
+Take a "cdp neighbors" structure created by parse genie.  Return the local interfaces pointing to "host"
+"""
+
+
+def host_to_ports(neighbors, host):
+    """
+    Take a cdp neighbors structure created by parse genie and return the interfaces pointing to "host"
+    :param neighbors: The cdp neighbors structure created by parse genie
+    :param host: The host we want to find interfaces for
+    :return: A list of interfaces connected to "host"
+    """
+    ports = []
+    for ndx in neighbors["cdp"]["index"]:
+        if str(host).lower() in str(neighbors["cdp"]["index"][ndx]["device_id"]).lower():
+            # print(neighbors["cdp"]["index"][ndx]["local_interface"])
+            ports.append(neighbors["cdp"]["index"][ndx]["local_interface"])
+    return ports
+
+
 class FilterModule(object):
     def filters(self):
+        """
+        Associate the filter with a specific function
+        :return: the filter-to-function association
+        """
         return {
-            'neighbor_ports': self.host_to_ports,
+            'neighbor_ports': host_to_ports,
         }
-
-    def host_to_ports(self, neighbors, host):
-        ports = []
-        for ndx in neighbors["cdp"]["index"]:
-            if str(host).lower() in str(neighbors["cdp"]["index"][ndx]["device_id"]).lower():
-                # print(neighbors["cdp"]["index"][ndx]["local_interface"])
-                ports.append(neighbors["cdp"]["index"][ndx]["local_interface"])
-        return ports
 
