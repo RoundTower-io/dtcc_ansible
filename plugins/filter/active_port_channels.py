905da707e235a1d4cf04370d6b9244aedec100bf
gamename <tennis.n.smith@gmail.com>
2019-09-16 08:41:50 -0500

    Add active port channels filter

---
+++ plugins/filter/active_port_channels.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+"""
+
+"""
+
+
+def find_port_channels(summary, interfaces):
+    """
+    """
+    active = []
+    for interface in interfaces:
+        print(interface)
+        if summary["interfaces"][interface]["oper_status"] == "up":
+            print("appending " + interface)
+            active.append(interface)
+    return active
+
+
+class FilterModule(object):
+    def filters(self):
+        """
+        Associate the filter with a specific function
+        :return: the filter-to-function association
+        """
+        return {
+            'neighbor_ports': find_port_channels,
+        }
+
