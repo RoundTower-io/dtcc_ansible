8e72b8a2b166d35fb0d3bd10a929e69364f178be
gamename <tennis.n.smith@gmail.com>
2019-10-29 12:35:47 -0400

    inline scripting updates

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,17 +1,17 @@
-pipeline {
-    agent "spvlanautomation01.lab.dtcc.com"
+node("spvlanautomation01.lab.dtcc.com") {
     parameters {
         string(name: 'VLAN', defaultValue: '', description: '')
     }
-    stages {
-        stage("Git Clone") {
-            steps {
-                sh 'https_proxy="https://172.26.136.21:80" git clone https://github.com/RoundTower-io/dtcc_ansible.git'
-            }
-        }
-        stage("Find VLAN File") {
-                echo "VLAN is ${params.VLAN}"
-                sh 'pwd; ls '
-        }
+    stage("PreClean") {
+        echo "Looking for old directories"
+        sh 'pwd; ls'
+        sh 'if [ -d "dtcc_ansible" ]; then rm -rf "dtcc_ansible"; fi'
+    }
+    stage("Git Clone") {
+       sh 'https_proxy="https://172.26.136.21:80" git clone https://github.com/RoundTower-io/dtcc_ansible.git'
+    }
+    stage("Find VLAN File") {
+        echo "VLAN is ${params.VLAN}"
+        sh 'pwd; ls '
     }
 }
