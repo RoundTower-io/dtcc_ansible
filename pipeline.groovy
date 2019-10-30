413ef5e64051f95e44eefa1aa3255923dcbc741f
gamename <tennis.n.smith@gmail.com>
2019-10-30 14:44:17 -0400

    first working version

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,4 +1,4 @@
-node("spvlanautomation01.lab.dtcc.com") {
+node("Ansible") {
     parameters {
         string(name: 'VLAN', defaultValue: '', description: '')
     }
@@ -8,10 +8,13 @@ node("spvlanautomation01.lab.dtcc.com") {
         sh 'if [ -d "dtcc_ansible" ]; then rm -rf "dtcc_ansible"; fi'
     }
     stage("Git Clone") {
-       sh 'https_proxy="https://172.26.136.21:80" git clone https://github.com/RoundTower-io/dtcc_ansible.git'
+       sh 'https_proxy="https://172.26.136.21:80" git clone --recurse-submodules https://github.com/RoundTower-io/dtcc_ansible.git'
     }
     stage("Find VLAN File") {
         echo "VLAN is ${params.VLAN}"
-        sh 'pwd; ls '
+        if (fileExists("dtcc_ansible/test_vlans/dtcc/vlan_${params.VLAN}.yml")) {
+            sh "cd dtcc_ansible; /usr/local/bin/ansible-playbook -i ./dtcc_hosts ./create_vlan.yml --extra-vars=\'@test_vlans/dtcc/vlan_${params.VLAN}.yml\'"
+        }
     }
 }
+
