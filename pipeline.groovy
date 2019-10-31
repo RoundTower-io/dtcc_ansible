ad5222c1d3488b52f80614051fb35f0e8ecfd602
gamename <tennis.n.smith@gmail.com>
2019-10-31 05:31:09 -0400

    working version

--- pipeline.groovy
+++ pipeline.groovy
@@ -10,10 +10,13 @@ node("Ansible") {
     stage("Git Clone") {
        sh 'https_proxy="https://172.26.136.21:80" git clone --recurse-submodules https://github.com/RoundTower-io/dtcc_ansible.git'
     }
-    stage("Find VLAN File") {
+    stage("Run Ansible") {
         echo "VLAN is ${params.VLAN}"
         if (fileExists("dtcc_ansible/test_vlans/dtcc/vlan_${params.VLAN}.yml")) {
+            
             sh "cd dtcc_ansible; /usr/local/bin/ansible-playbook -i ./dtcc_hosts ./create_vlan.yml --extra-vars=\'@test_vlans/dtcc/vlan_${params.VLAN}.yml\'"
+        } else {
+            error("Cannot find any configuration file for VLAN ${params.VLAN}")
         }
     }
 }
